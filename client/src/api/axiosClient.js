e7411d9e5a41984979ecdaf6203922ff084224e2
DvanXanMalai <xabilal27@gmail.com>
2026-05-02 09:09:05 +0545

    connect frontend to prod backend

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://vaccination-tracker-7xqj.onrender.com',
   withCredentials: true,
 });
 
